be85797d44cb5bf3adcbc979921255fe11d64f24
jianchengxu <jianchengxu@sohu-inc.com>
2018-12-10 21:47:46 +0800

    首次提交

---
+++ guess-num/src/lucky/shiyiyi/guessnum/GuessNumFrameRunner.java
@@ -0,0 +1,19 @@
+package lucky.shiyiyi.guessnum;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class GuessNumFrameRunner {
+
+	public static void main(String[] args) {
+		EventQueue.invokeLater(()->{
+			JFrame frame = new GuessNumFrame();
+			frame.setTitle("小谢的猜数字游戏");
+			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			frame.setSize(400, 300);
+			frame.setVisible(true);
+		});
+	}
+
+}
